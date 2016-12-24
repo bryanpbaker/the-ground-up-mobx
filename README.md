940216462b988b1a7af545048a8f330f1160e0f3
Bryan Baker <bryanpbaker@gmail.com>
2016-12-24 10:08:40 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>The Ground Up Mobx</h1>
+
+<h2>A bad to the bone starter kit for React and Mobx apps. Uses Webpack, Babel, Capistrano, Sass and some other cool stuff.</h2>
+
+<h2>Getting Started</h2>
+<ul>
+    <li>Clone this repo</li>
+    <li><code>cd the-ground-up-mobx</code></li>
+    <li><code>npm install</code></li>
+    <li><code>npm start</code></li>
+</ul>
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+
+import Header from './components/header.component';
+import Test from './components/test.component';
+
+import '../style/main.scss';
+
+const App = (props) => {
+
+    return(
+        <div className="app">
+            <Header/>
+            <Link to="/">Home</Link> &nbsp; <Link to="test">Test Route</Link>
+            {props.children}
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="test" component={Test}></Route>
+        </Route>
+    </Router>
+, document.querySelector('.container'));
